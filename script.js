13b5faf4d0c8877f55193d2d6e2ce2d1a36f7cdc
Md. Razikul Islam Joni <mdrazikulislamjoni@gmail.com>
2021-02-13 23:28:38 +0600

    second commit

--- script.js
+++ script.js
@@ -12,27 +12,39 @@ const searchItems = () => {
     const url = `https://www.themealdb.com/api/json/v1/1/search.php?f=${searchField}`;
     fetch(url)
         .then((response) => response.json())
-        .then((data) => showItems(data.strMeal));
+        .then((data) => showItems(data.meals));
 };
 //Showing Items
-const ShowItems = (items) => {
+const showItems = (items) => {
     const foodsContainer = document.getElementById("food-container");
     foodsContainer.innerHTML = "";
     items.forEach((singleItem) => {
         const foodsDiv = document.createElement("div");
         foodsDiv.className = "food-items";
-        foodsDiv.innerHTML = `<div class='food-box'>
-        <div class='food-icon'><img src='${singleItem.strMealThumb}'></div>
-        <div class='food-name'><h2>${singleItem.strMeal}</h2></div>
-        </div>`;
+        foodsDiv.innerHTML = 
+                            `<div onclick="getItemDetails('${items[0].strMeal}');" class='food-box'>
+                            <div class='food-icon'><img src='${singleItem.strMealThumb}'></div>
+                            <div class='food-name'><h2>${singleItem.strMeal}</h2></div>`;
         foodsContainer.appendChild(foodsDiv);
+        // getItemDetails(items[0].strMeal);
+        console.log(items);
     });
 };
 //Item Details
-const getItemDetails = async (name) =>{
-    const foodDetails = document.getElementById("food-details")
-    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${name}`
-    const res = await fetch(url)
+const getItemDetails = async (name) => {
+    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${name}`;
+    const res = await fetch(url);
     const data = await res.json();
-    console.log(data);
-}
+    showItemDetails(data);
+};
+//show item details
+const showItemDetails = (details) => {
+    console.log(details);
+    const foodDetails = document.getElementById("food-details");
+    const detailsDiv = document.createElement("div");
+    detailsDiv.innerHTML = `<img src="${details.meals[0].strMealThumb}">
+                            <div class="details-name">${details.meals[0].strMeal}</div>
+                            <div class="details-ingredient">${details.meals[0].strIngredient1}</div>
+                            <div class="details-ingredient">${details.meals[0].strIngredient10}</div>`;
+    foodDetails.appendChild(detailsDiv);
+};
